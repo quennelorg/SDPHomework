0f4bb26450efc584a4752c36acb866f150b3767f
xinyuji <quennel1115@gmail.com>
2024-05-21 15:49:28 +0800

    feat: add edge case

--- TDD/src/test/java/BowlingGameTest.java
+++ TDD/src/test/java/BowlingGameTest.java
@@ -12,4 +12,13 @@ public class BowlingGameTest {
         }
         assertEquals(0, game.getScore()); // 总得分应该为0
     }
+
+    @Test
+    public void testAllOnes() {
+        BowlingGame game = new BowlingGame();
+        for (int i = 0; i < 20; i++) {
+            game.roll(1); // 每次扔球击倒一个球瓶
+        }
+        assertEquals(20, game.getScore()); // 总得分应该为20
+    }
 }
